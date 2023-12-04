ae5e0beff4a7b8dafa5e4feda54d41cfce458cba
lee <lkboisa@yahoo.com>
2023-12-04 12:53:04 -0800

    stage 1, both dad and tylers 1.0 python scripts

---
+++ lee1.0.py
@@ -0,0 +1,21 @@
+#network chuck rocks
+
+#print("this is a good test")
+#print("just some practice")
+#print("no need to shout")
+
+#print("I am ironman. \n"+"No, I am Tony Stark.\n" + "No actually I'm Lee.")
+#print("<3" * 10)
+
+#print(input("what is your name"))
+
+print("Welcome to my lab")
+name = input("What is your name \n")
+print("\nWelcome " + name + ", Thank you for joining us")
+input("To continue press enter \n")
+
+menu = "Black coffee, Espresso, Latte, Cappucino"
+
+print(name + ", What sort of coffee would you like to try today? \n" + menu)
+order = input()
+print("Thank You " + name + ".\nWe'll have that " + order + " ready for you in a minute") 
---
+++ tyler1.0.py
@@ -0,0 +1,6 @@
+#Tyler and Dad's python script
+print("Welcome to Tyler and Dad's lab at home")
+name = input("what is your name?\n")
+
+
+print("Hello " + name + ", Welcome to our lab")
